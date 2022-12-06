54a7550f068bc37c3200bf9518e8e04322cde47d
yulirojaslon930 <yrojasl@unal.edu.co>
2022-12-05 22:17:33 -0500

    preguntas

--- preguntas.py
+++ preguntas.py
@@ -1,77 +1,116 @@
 """
-Clasificación usando k-NN
+Clasificación usando k-NN - Digits Dataset
 -----------------------------------------------------------------------------------------
+En este laboratio se construirá un clasificador usando k-NN para el dataset de digitos.
 """
+import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-
+from sklearn import datasets
 
 def pregunta_01():
     """
     Complete el código presentado a continuación.
     """
-    # Lea el archivo de datos
-    df = pd.read_csv("house-votes-84.csv", sep=",")
 
-    # Cree un vector con la variable de respuesta ('party')
-    y = df['party'].values
+    # Cargue el dataset digits
+    digits = datasets.load_digits()
 
-    # Extraiga las variables de entrada
-    X = df.drop('party', axis=1).values
+    # Imprima los nombres de la variable target del dataset
+    print(digits.target_names)
 
-    # Importe el transformador OrdinalEncoder
-    from sklearn.preprocessing import OrdinalEncoder
+    # Imprima las dimensinoes de matriz de datos
+    print(digits.data.shape)
 
-    # Transforme las variables de entrada usando fit_transform
-    X = OrdinalEncoder().fit_transform(X)
-
-    # Importe KNeighborsClassifier de sklearn.neighbors
-    from sklearn.neighbors import KNeighborsClassifier
-    
-
-    # Cree un un clasificador k-NN con 6 vecinos
-    knn = KNeighborsClassifier(n_neighbors=6) 
-
-    # Entrene el clasificador con el conjunto de entrenamiento
-    knn.fit(X, y)  
-
-    # Retorne el score del clasificador
-    return knn.score(X, y).round(3) 
+    # Imprima las dimensiones del vector de salida
+    print(digits.target.shape)
 
 
 def pregunta_02():
     """
     Complete el código presentado a continuación.
     """
-    # Lea el archivo de datos
-    df = pd.read_csv("house-votes-84.csv", sep=",")
+    # Importe KNeighborsClassifier de sklearn.neighbors
+    from sklearn.neighbors import KNeighborsClassifier
 
-    # Cree un vector con la variable de respuesta ('party')
-    y = df['party'].values
+    # Importe train_test_split de sklearn.model_selection
+    from sklearn.model_selection import train_test_split
 
-    # Extraiga las variables de entrada
-    X = df.drop('party', axis=1).values
+    # Cargue el dataset digits
+    digits = datasets.load_digits()
 
-    # Importe el transformador OrdinalEncoder
-    from sklearn.preprocessing import OrdinalEncoder
+    # Cree los vectors de características y de salida
+    X = digits.data
+    y = digits.target
 
-    # Transforme las variables de entrada usando fit_transform
-    X = OrdinalEncoder().fit_transform(X)
+    # Divida los datos de entrenamiento y prueba. Los conjuntos de datos están
+    # estratificados. La semilla del generador de números aleatorios es 42.
+    # El tamaño del test es del 20%
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42, stratify=y
+    )
+
+    # Cree un clasificador con siete vecinos
+    knn = KNeighborsClassifier(n_neighbors=7)
+
+    # Entrene el clasificador
+    knn.fit(X_train, y_train)
+
+    # Imprima la precisión (score) del clasificador en el conjunto de datos de prueba
+    print(round(knn.score(X_test, y_test), 4))
+
+
+def pregunta_03():
+    """
+    Complete el código presentado a continuación.
+    """
 
     # Importe KNeighborsClassifier de sklearn.neighbors
     from sklearn.neighbors import KNeighborsClassifier
-    
 
-    # Cree un un clasificador k-NN con 6 vecinos
-    knn = KNeighborsClassifier(n_neighbors=5)
+    # Importe train_test_split de sklearn.model_selection
+    from sklearn.model_selection import train_test_split
 
-    # Entrene el clasificador con el conjunto de entrenamiento
-    knn.fit(X, y)
+    # Cargue el dataset digits
+    digits = datasets.load_digits()
 
-    # Pronostique el resultado para el conjunto de entrenamiento
-    y_pred = knn.predict(X)
+    # Cree los vectors de características y de salida
+    X = digits.data
+    y = digits.target
 
-    # Importe la función confusion_matrix de sklearn.metrics
-    from sklearn.metrics import confusion_matrix
+    # Divida los datos de entrenamiento y prueba. Los conjuntos de datos están
+    # estratificados. La semilla del generador de números aleatorios es 42.
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y , test_size=0.2, random_state=42, stratify=y
+    )
 
-    # Retorne la matriz de confusión
-    return confusion_matrix(y, y_pred)
+    # Inicialice los arreglos para almacenar la precisión para las muestras de
+    # entrenamiento y de prueba
+    neighbors = np.arange(1, 9)
+    train_accuracy = np.empty(len(neighbors))
+    test_accuracy = np.empty(len(neighbors))
+
+    # Se itera sobre diferentes valores de vecinos
+    for i, k in enumerate(neighbors):
+        # Cree un clasificador con k vecinos
+        knn = KNeighborsClassifier(n_neighbors=k)
+
+        # Entrene el clasificador con los datos de entrenamiento
+        knn.fit(X_train, y_train)
+
+        # Calcule la precisión para el conjunto de datos de entrenamiento
+        train_accuracy[i] = knn.score(X_train, y_train)
+
+        # Calcule la precisión para el conjunto de datos de prueba
+        test_accuracy[i] = knn.score(X_test, y_test)
+
+    # Almacenamiento de los resultados como un dataframe
+    df = pd.DataFrame(
+        {
+            "k": neighbors,
+            "train_accuracy": train_accuracy,
+            "test_accuracy": test_accuracy,
+        }
+    )
+
+    return df
